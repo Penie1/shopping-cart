4f236a601cf65ad390f6333165e162de20fe40ff
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-11-24 15:35:03 +0300

    Implement lazy loading for images

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,14 +1,20 @@
 import PropTypes from "prop-types";
 import { ShoppingCartIcon } from "lucide-react";
 import shortProductName from "../utilities/shortProductName";
-
 import { useOutletContext } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function ProductCards({ id, image, title, price }) {
   const { addItemsToCart } = useOutletContext();
   return (
     <div className="bg-white border relative border-neutral-200 rounded-md flex flex-col gap-6 justify-center items-center py-10  px-6 tracking-wider">
-      <img src={image} alt={title} className=" w-auto h-44" />
+      <LazyLoadImage
+        effect="blur"
+        src={image}
+        alt={title}
+        className=" w-auto h-44"
+      />
       <h4 className="text-sm text-neutral-700 text-center font-medium">
         {shortProductName(title)}
       </h4>
@@ -17,6 +23,7 @@ function ProductCards({ id, image, title, price }) {
       <button
         onClick={() => addItemsToCart(id)}
         className="flex gap-2 items-end bg-neutral-600 hover:bg-neutral-800 transition rounded-md text-yellow-500 px-3 py-2"
+        aria-label={`Add ${title} to cart`}
       >
         <span>
           <ShoppingCartIcon size={18} />
